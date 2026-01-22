8cd7d6cb9813eee750cb2f8bea7d32a6aa1eb172
Iamwindy5 <dangtuyentc25@gmail.com>
2026-01-22 16:09:33 +0700

    Demo Part 2 SpaceShooter - AutoShoot

---
+++ Assets/Space Shooter Template FREE/Scripts/MyBullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBullet : MonoBehaviour
+{
+    public float flySpeed;
+    void Update()
+    { 
+        Vector3 newPosition = transform.position;
+        newPosition.y += flySpeed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
+
